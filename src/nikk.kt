30831f5da974713ef70e62230d358a16438657f7
nikhilpatil <nikhilpatil1050@gmail.com>
2021-08-01 05:10:32 -0700

    nikk file

---
+++ src/nikk.kt
@@ -0,0 +1,17 @@
+class nikk
+{
+
+   fun  dis(num:Int, num2:Int) :Int {
+      var  sum=num+num2;
+       print(sum)
+      return sum;
+
+   }
+}
+
+fun main() {
+    var s=nikk();
+    s.dis(10,20)
+
+}
+
