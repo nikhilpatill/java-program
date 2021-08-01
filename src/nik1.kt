13603c65c207a0926f160f3ec8267de81c04b9a5
nikhilpatil <nikhilpatil1050@gmail.com>
2021-07-31 23:20:04 -0700

    Initial commit

---
+++ src/nik1.kt
@@ -0,0 +1,8 @@
+import kotlin.jvm.JvmStatic
+
+object nik {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(" nikhil")
+    }
+}
---
+++ src/oops/oo.kt
@@ -0,0 +1,17 @@
+package oops
+
+class oo {
+    fun dis()
+    {
+        print(" hello");
+        print(" nikhil2")
+
+    }
+
+
+}
+
+fun main() {
+    var s=oo();
+    s.dis()
+}
