3259a4ca1469ab1eb273c08fba1573feaff0a26e
nikhilpatil <nikhilpatil1050@gmail.com>
2021-08-01 04:29:49 -0700

    first commit

---
+++ src/oops/fod.kt
@@ -0,0 +1,18 @@
+package oops
+
+class fod (num:Int, name:String ) {
+    var e_num = num
+    var e_name = name
+
+    init {
+
+
+        print(" enter ther number "+num);
+        print(" enter the umber "+name)
+    }
+}
+
+fun main() {
+    var s=fod(101,"nikhil");
+}
+
---
+++ src/oops/jo.kt
@@ -0,0 +1,14 @@
+package oops
+
+class jo {
+    fun dis() {
+        var a1 = arrayOf(10, 20, 30, 40, 50,)
+        var min = arrayOf(0)
+    }
+}
+
+fun main() {
+    var s1 = jo()
+    s1.dis()
+
+}
