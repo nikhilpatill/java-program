d89ff0b7267f32394e92f437a68ea6fbae641ff3
nikhilpatil <nikhilpatil1050@gmail.com>
2021-08-02 11:09:41 +0530

    add third line

--- src/nikhil.java
+++ src/nikhil.java
@@ -1,9 +1,2 @@
 public class nikhil {
-
-    public static void main(String[] args) {
-        System.out.println("nikhil");
-        System.out.println("patil ");
-        System.out.println("age 12");
-
-    }
 }
